e2dd70af8311b166846672e15c968f4d0fffb4ec
Your Name <you@example.com>
2023-11-24 21:57:19 -0300

    Primeira versão

---
+++ loops.py
@@ -0,0 +1,22 @@
+"""
+clientes=[]
+clientes.append("Marcus")
+clientes.append("Gustavo")
+clientes.append("Guilherme")
+
+numero=0
+for x in clientes:
+    print("Cliente:", x," Numero:",numero)
+    numero=numero+1
+
+"""
+
+clientes=[]
+clientes.append("Marcus")
+clientes.append("Gustavo")
+clientes.append("Guilherme")
+
+numero=0
+while numero != len(clientes):
+    print("Cliente:",clientes[numero]," Numero:",numero)
+    numero=numero+1
